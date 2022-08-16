1aa6ead1f378170a9720706869a352ee305a95c8
Tóth Bálint <toth.balint1225@gmail.com>
2022-08-16 19:58:18 +0200

    Alacritty config, prompt and aliases

---
+++ applauncher.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# green
+# dmenu_run -bo 3 -c -l 10 -p '>' -nb '#282A36' -nf '#98be65' -sb '#98be65' -sf '#282A36'
+
+# purple
+dmenu_run -bo 3 -c -l 10 -p '>' -nb '#282A36' -nf '#c792ea' -sb '#c792ea' -sf '#282A36'
